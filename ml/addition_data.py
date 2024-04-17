407dedc7667eec4633e7d924b771fae50f4ba1b4
cutterdawes <cutter@dawes.org>
2024-04-17 18:28:56 -0400

    Test with GPU and 32 CPU workers

--- ml/addition_data.py
+++ ml/addition_data.py
@@ -188,10 +188,10 @@ def prepare(
     
     # create training dataset and dataloader
     training_dataset = BaseAddition(table, depth, ids=ids, interleaved=True, digit_order='reversed')
-    training_dataloader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True) #****** num_workers not passed
+    training_dataloader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers) #****** num_workers passed
     
     # create testing dataset and dataloader
     testing_dataset = BaseAddition(table, depth, ids=heldout_ids, interleaved=True, digit_order='reversed')
-    testing_dataloader = DataLoader(testing_dataset, shuffle=True) #****** num_workers not passed
+    testing_dataloader = DataLoader(testing_dataset, shuffle=True, num_workers=num_workers) #****** num_workers passed
 
     return training_dataloader, testing_dataloader
