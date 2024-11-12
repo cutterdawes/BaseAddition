4d440aa5856261a96703b850d64cfc17db18d435
cutterdawes <cutter@dawes.org>
2024-11-12 14:04:10 -0500

    .

--- statistical/associativity.py
+++ statistical/associativity.py
@@ -28,35 +28,43 @@ def main():
 
         # iterate through tables
         for dc, table in all_tables[b].items():
-            tuples = list(product(*[range(b)]*args.depth))
-            if args.depth > 3:
-                tuples = random.sample(tuples, b**3)
-            num_assoc = 0
-            triplets = list(combinations(tuples, 3))
-            for (n, m, p) in triplets:
-        
-                # convert to group elements
-                n = BaseElt(n, table)
-                m = BaseElt(m, table)
-                p = BaseElt(p, table)
-        
-                # check associativity
-                s1 = (n + m) + p
-                s2 = n + (m + p)
-                is_assoc = s1.vals == s2.vals
-                if is_assoc:
-                    num_assoc += 1
+            
+            # iterate through depths
+            assoc_vs_depth = []
+            for depth in range(2, args.depth + 1):
+                tuples = list(product(*[range(b)]*depth))
+                if depth > 3:
+                    tuples = random.sample(tuples, b**3)
+                num_assoc = 0
+                triplets = list(combinations(tuples, 3))
+                for (n, m, p) in triplets:
+            
+                    # convert to group elements
+                    n = BaseElt(n, table)
+                    m = BaseElt(m, table)
+                    p = BaseElt(p, table)
+            
+                    # check associativity
+                    s1 = (n + m) + p
+                    s2 = n + (m + p)
+                    is_assoc = s1.vals == s2.vals
+                    if is_assoc:
+                        num_assoc += 1
+
+                # calculate associativity
+                assoc = num_assoc / len(triplets)
+                assoc_vs_depth.append(assoc)
         
             # add fraction of associativity to dictionary
-            associativity[dc] = num_assoc / len(triplets)
+            associativity[dc] = assoc_vs_depth
 
         # add to overall dictionary
         all_associativity[b] = associativity
         print('complete\n')
 
-        # pickle overall dictionary
-        with open(f'../pickles/complexity_measures/associativity_d{args.depth}.pickle', 'wb') as f:
-            pickle.dump(all_associativity, f)
+    # pickle overall dictionary
+    with open(f'../pickles/complexity_measures/associativity_vs_depth_d{args.depth}.pickle', 'wb') as f:
+        pickle.dump(all_associativity, f)
 
 
 if __name__ == '__main__':
